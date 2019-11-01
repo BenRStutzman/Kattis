07c645fd6491809c032195310e4e16138659611b
BenRStutzman <tysorx89@gmail.com>
2019-11-01 00:08:35 -0400

    more problems

--- 2019 Practice 2/incrementalinduction.py
+++ 2019 Practice 2/incrementalinduction.py
@@ -8,29 +8,36 @@ def main():
     for i in range(N):
         scores[i] = results[i].count("1") + len([1 for x in range(i + 1, N) if results[x][i] == "0"])
     losses = sorted([N - 1 - score for score in scores])
-    print(losses)
+    #print(losses)
     k = 0
     last_losses = -1
     past = 0
     cur_bad = 0
     last_tier = 0
+    on_this_tier = 0
+    past = 0
+    delayed = 0
     for i in range(N):
         if losses[i] == last_losses:
-            #print("I:", i)
-            if last_tier == 0:
-                cur_bad += losses[i] - i
-            else:
-                cur_bad += losses[i] - losses[last_tier] - (i - last_tier)
+            on_this_tier += 1
+            cur_bad += losses[i] - past - (i - last_tier)
+            if delayed:
+                past += 1
+                delayed = 0
         else:
-            if last_tier == 0:
-                cur_bad = losses[i]
+            if on_this_tier % 2 == 0 and on_this_tier > 1:
+                past += on_this_tier - 1
+                delayed = 1
             else:
-                cur_bad = losses[i] - losses[last_tier]
+                past += on_this_tier
+            on_this_tier = 1
+            cur_bad = losses[i] - past
             last_tier = i
             last_losses = losses[i]
         if cur_bad > k:
                 k = cur_bad
-        print(cur_bad)
+        print("past", past)
+        print("cur_bad", cur_bad)
     print(k)
 
             
--- Open Kattis/compoundwords.py
+++ Open Kattis/compoundwords.py
@@ -1,13 +1,17 @@
-import sys
+from sys import stdin
+from itertools import product
 
 def main():
-    inp = sys.stdin.read().splitlines()
-    N = int(inp[0])
-    for i, bit in enumerate(inp[1]):
-        if (bit == inp[2][i]) == (N % 2):
-            print("Deletion failed")
-            break
-    else:
-        print("Deletion succeeded")
+    inp = [line.split() for line in stdin.read().splitlines()]
+    words = set()
+    for line in inp:
+        for word in line:
+            words.add(word)
+    combos = set()
+    for A, B in product(words, repeat = 2):
+        if A != B:
+            combos.add(A + B)
+    for word in sorted(combos):
+        print(word)
 
 main()
